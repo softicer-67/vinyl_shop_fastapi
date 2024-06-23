ee8dabdd15d8a54fa91bc967ebf1acdbfe37bc78
Yuri <57116431+softicer-67@users.noreply.github.com>
2024-06-24 02:01:45 +0500

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from config.settings import Base, engine
 
+from apps.categories.routers import app as category_app
+from apps.brands.routers import app as brand_app
+from apps.products.routers import app as product_app
 
 # Создаем экземпляр FastAPI
 app = FastAPI()
@@ -12,13 +15,16 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 # Настройка CORS
-origin_cors = ['*']
+origins = ['*']
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origin_cors,
+    allow_origins=origins,
+    # allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
 )
+
 
 @app.on_event('startup')
 async def startup():
@@ -27,10 +33,6 @@ async def startup():
 
 
 # Добавляем подприложения FastAPI
-from apps.categories.routers import app as category_app
-from apps.brands.routers import app as brand_app
-from apps.products.routers import app as product_app
-
 app.include_router(category_app)
 app.include_router(brand_app)
 app.include_router(product_app)
